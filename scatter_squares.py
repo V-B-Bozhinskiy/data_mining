10517f1b8cc921db00cb03729e842c0e4ae3f3ad
V-B-Bozhinskiy <bove203@outlook.com>
2025-11-02 13:23:36 +0300

    scatter_calcValue

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 #Назначение заголовка диаграмы и меток осей
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Назначение размера шрифта делений на осях
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# назначение диапазона для каждой оси
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
