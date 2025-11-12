18b691714503c3e1024b6943a7179da81b55d961
V-B-Bozhinskiy <bove203@outlook.com>
2025-11-12 18:20:51 +0300

    random_walk_cmap3

--- rw_visual.py
+++ rw_visual.py
@@ -6,18 +6,22 @@ from random_walk import RandomWalk
 
 while True:
     # Построение случайного блуждания
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Нанесение точек на диаграмму
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     points_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=points_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=points_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     
     # Выделение первой и последней точки
     ax.scatter(0,0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Удаление осей
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     
     plt.show()
 
