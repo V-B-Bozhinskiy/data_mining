c466ea5d43dddbce8a0f96d522f730dfce47e531
V-B-Bozhinskiy <bove203@outlook.com>
2025-11-15 13:28:16 +0300

    die_2

--- die_visual.py
+++ die_visual.py
@@ -4,30 +4,32 @@ from plotly import offline
 from die import Die
 
 #Создание кубика Д6
-die = Die() #D6 по умолчанию
+die_1 = Die() #D6 по умолчанию
+die_2 = Die()
 
 # Моделирование серии бросков с сохранением результата в списке.
 results = []
 for roll_num in range(100):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 print(results)
 
 # Анализ результатов.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 print(frequencies)
 
 # Визуализация результатов
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2,max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Result of rollnig one D6 100 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+my_layout = Layout(title='Result of rollnig two D6 100 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
